057db371889ffa645a69d2807c417aac34b666dc
Nicola Reggiani <nicola.reggiani@reiteco.com>
2020-06-16 17:39:30 +0200

    Moved exec args into command

--- deploy.js
+++ deploy.js
@@ -3,17 +3,16 @@ const exec = require("@actions/exec");
 let deploy = function (folder, bucket, distId) {
   return new Promise((resolve, reject) => {
     try {
-      const args = [
-        `./${folder}/**`,
-        `--bucket ${bucket}`,
-        `--cwd './${folder}'`,
-        `--distId ${distId}`,
-        `--etag`,
-        `--gzip xml,html,htm,js,css,ttf,otf,svg,txt`,
-        `--invalidate '/'`,
-        `--noCache`,
-      ];
-      exec.exec("npx s3-deploy@1.4.0", args).then(resolve).catch(reject);
+      const command = `npx s3-deploy@1.4.0 './${folder}/**' \
+                        --bucket ${bucket} \
+                        --cwd './${folder}' \
+                        --distId ${distId} \
+                        --etag \
+                        --gzip xml,html,htm,js,css,ttf,otf,svg,txt \
+                        --invalidate / \
+                        --noCache `;
+
+      exec.exec(command).then(resolve).catch(reject);
     } catch (e) {
       reject(e);
     }
