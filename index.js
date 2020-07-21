154ac1b16461acea1c0d115bd8c9e72e5f33c19b
Allan Fernandes <alnmaiafernandes@gmail.com>
2020-07-21 18:30:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ async function run() {
   try {
     const folder = core.getInput('folder');
     const bucket = core.getInput('bucket');
+    const bucketRegion = core.getInput('bucket-region');
     const distId = core.getInput('dist-id');
 
-    await deploy(folder, bucket, distId);
+    await deploy(folder, bucket, bucketRegion, distId);
   }
   catch (error) {
     core.setFailed(error.message);
