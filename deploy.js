af6427ee563f7191cb51e07d6c14909fa9c79a93
Cullan <cullan.luther@gmail.com>
2022-08-16 21:49:06 -0500

    changing gzip to brotli

--- deploy.js
+++ deploy.js
@@ -25,7 +25,7 @@ let deploy = function (params) {
                         --cwd ./ \
                         ${distIdArg} \
                         --etag \
-                        --gzip xml,html,htm,js,css,ttf,otf,svg,txt \
+                        --br xml,html,htm,js,css,ttf,otf,svg,txt \
                         ${cacheFlag} \
                         ${invalidationArg} \
                         ${deleteRemovedArg} \
